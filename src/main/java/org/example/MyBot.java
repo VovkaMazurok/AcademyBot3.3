2876d35a01a30aab89fcba6cfe3d23e9a9386cf1
Volodymyre <vova89.89@i.ua>
2024-07-29 13:30:53 +0300

    Initial commit

---
+++ src/main/java/org/example/MyBot.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import net.thauvin.erik.crypto.CryptoPrice;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramLongPollingBot {
+
+    public MyBot() {
+        super("6644785668:AAFa6w5npf1MCR_DLu2-NR0Kclytfe2URH0");
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var chatId = update.getMessage().getChatId();
+        var text = update.getMessage().getText();
+
+        try {
+            var message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Hello");
+
+            if (text.equals("/start")) {
+                message.setText("Hi");
+            } else if (text.equals("btc")) {
+                var price = CryptoPrice.spotPrice("BTC");
+                message.setText("BTC price: " + price.getAmount().doubleValue());
+            } else if (text.equals("eth")) {
+                var price = CryptoPrice.spotPrice("ETH");
+                message.setText("ETH price: " + price.getAmount().doubleValue());
+            } else if (text.equals("doge")) {
+                var price = CryptoPrice.spotPrice("DOGE");
+                message.setText("Dхм price: " + price.getAmount().doubleValue());
+            } else if (text.equals("all")) {
+                var price = CryptoPrice.spotPrice("BTC");
+                var price1 = CryptoPrice.spotPrice("ETH");
+                var price2 = CryptoPrice.spotPrice("DOGE");
+                message.setText("BTC price: " + price.getAmount().doubleValue()
+                        + "\nETH price: " + price1.getAmount().doubleValue()
+                        + "\nDogecoin price: " + price2.getAmount().doubleValue());
+            } else {
+                message.setText("Unknown command!");
+            }
+            execute(message);
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
+
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Prog333_bot";
+    }
+}
